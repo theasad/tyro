95227833d6be6d4cfb90071a5adfb87779698133
Hasin Hayder <countdraculla@gmail.com>
2025-11-23 13:43:15 +0530

    hasPrivilege() and hasPrivileges() in Role model

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Role extends Model
-{
+class Role extends Model {
     use HasFactory;
 
     protected $fillable = ['name', 'slug'];
@@ -16,18 +15,44 @@ class Role extends Model
 
     protected $table = 'roles';
 
-    public function users()
-    {
+    public function users() {
         $userClass = config('tyro.models.user', config('auth.providers.users.model', 'App\\Models\\User'));
 
         return $this->belongsToMany($userClass, config('tyro.tables.pivot', 'user_roles'));
     }
 
-    public function privileges(): BelongsToMany
-    {
+    public function privileges(): BelongsToMany {
         return $this->belongsToMany(
             Privilege::class,
             config('tyro.tables.role_privilege', 'privilege_role')
         )->using(RolePrivilege::class)->withTimestamps();
+    }
+
+    /**
+     * Check if the role has a specific privilege by slug.
+     *
+     * @param string $privilegeSlug
+     * @return bool
+     */
+    public function hasPrivilege(string $privilegeSlug): bool {
+        return $this->privileges()->where('slug', $privilegeSlug)->exists();
+    }
+
+    /**
+     * Check if the role has all of the specified privileges by slug.
+     *
+     * @param array $privilegeSlugs
+     * @return bool
+     */
+    public function hasPrivileges(array $privilegeSlugs): bool {
+        $rolePrivilegeSlugs = $this->privileges()->pluck('slug')->toArray();
+
+        foreach ($privilegeSlugs as $slug) {
+            if (!in_array($slug, $rolePrivilegeSlugs)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
